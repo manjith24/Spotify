10146ba42b0597f3d11776fec2d58ebfef87fa1d
Sneha <sneha.mn@hrblock.com>
2025-01-06 22:12:01 +0530

    Initial Commit

---
+++ MusicApp/Models/MyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MusicApp.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+                
+        }
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    base.OnModelCreating(builder);
+        //}
+
+        public DbSet<User> Users 
+        {
+            get; set;
+        }
+    }  
+}
